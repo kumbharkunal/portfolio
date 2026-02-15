5fc0bb512230a71d7c36db58fde7504107bb1b43
kunal <kumbharkunaldaulat@gmail.com>
2026-02-15 13:15:52 +0530

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -139,7 +139,7 @@ const Hero = () => {
                                 </svg>
                             </motion.a>
                             <motion.a
-                                href="/resume.pdf?v=4"
+                                href="../../Kunal_Kumbhar_Full_Stack_Developer.pdf"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="btn-glass"
